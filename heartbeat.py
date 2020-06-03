ec8b62e55ea07f8af1e8d4059d8ffa9e0a191c49
plamistef <plamstef@abv.bg>
2020-06-03 14:42:52 +0200

    select sensor working

--- heartbeat.py
+++ heartbeat.py
@@ -5,68 +5,48 @@ import matplotlib.pyplot as plt
 import numpy as np
 import serial
 
-class heartbeat:
+class Heartbeat:
 
-    def draw_canvas(self):
-        plot_window = 20 
-        y_var = np.array(np.zeros([plot_window]))
-        plt.ion()
-        fig, ax = plt.subplots()
-        line, = ax.plot(y_var)
+    plot_window = 20 
+    y_var = np.array(np.zeros([plot_window]))
+    plt.ion()
+    fig, ax = plt.subplots()
+    line, = ax.plot(y_var)
 
-    def sound(self,ser):
-        draw_canvas()
+    def select_sensor (self, ser, name):
         while True:
-            try:
-                #receiving data from the arduino
-                ser_bytes = ser.readline()
-                #filter sound data
-                if 's' in ser_bytes:
-                    #cut off the first 3 characters of the string
-                    ser_bytes = ser_bytes[3:]
-            except:
-                print("interrupt")
-                break
+            ser_bytes = ser.readline().decode()   
+            if name[:1] in ser_bytes:
+                ser_bytes = ser_bytes[3:]
+                self.fill_data(ser_bytes,name)
 
-    def water(self,ser):
-        while True:
-            try:
-                #receiving data from the arduino
-                ser_bytes = ser.readline()
-                #filter sound data
-                if 'w' in ser_bytes:
-                    #cut off the first 3 characters of the string
-                    ser_bytes = ser_bytes[3:]
-            except:
-                print("interrupt")
-                break
-
-    def fill_data(self,ser_bytes):
-        decoded_bytes = float(ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
+    def fill_data(self,ser_bytes,graph_name):
+        decoded_bytes = float(ser_bytes[0:len(ser_bytes)-2])
         #saving the data on a csv file
-        with open("song_data.csv","a") as f:
+        with open("data.csv","a") as f:
             writer = csv.writer(f,delimiter=",")
-            writer.writerow([strftime("%c", gmtime()),decoded_bytes])
+            writer.writerow([strftime("%c", gmtime()),graph_name[:1],decoded_bytes])
         #appending the latest data to the graph
-        y_var = np.append(y_var,decoded_bytes)
-        y_var = y_var[1:plot_window + 1]
-        line.set_ydata(y_var)
-        ax.relim()
-        ax.autoscale_view()
+        self.y_var = np.append(self.y_var,decoded_bytes)
+        self.y_var = self.y_var[1:self.plot_window + 1]
+        self.line.set_ydata(self.y_var)
+        self.ax.relim()
+        self.ax.autoscale_view()
         #drawing the graph
-        plt.title(""" song heartbeat """)
-        fig.canvas.draw()
-        fig.canvas.flush_events()
+        plt.title(graph_name)
+        self.fig.canvas.draw()
+        self.fig.canvas.flush_events()
 
 def main():
+    pass
+
+if __name__ == "__main__":
+    hb = Heartbeat()
     #select serial port
     ser = serial.Serial('COM6',9600)
     ser.flushInput()
-    sensors = raw_input("please select a sensor:\n")
+    sensors = input("please select a sensor:\n")
     if sensors == "sound":
-        sound(ser)
+        hb.select_sensor(ser,"sound sensor data")
     elif sensors == "water":
-        water(ser)
-
-if __name__ == "__main__":
-    main ()
+        hb.select_sensor(ser,"water sensor data")
